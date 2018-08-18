12c1410e637473d8a2d108c425b8669f15bb69b2
pooi <ldayou@naver.com>
2018-08-18 11:40:04 +0900

    Add features   * Check last login date using database

--- util/db-dao.js
+++ util/db-dao.js
@@ -21,8 +21,18 @@ exports.createNewUser = function (userId, name, email, photo, provider, callback
         } else {
             if(results.length > 0){
                 console.log("Already registered user");
-                callback(true);
+                var sql = "UPDATE user SET name=?, email=?, photo=?, last_login_date=CURRENT_TIMESTAMP WHERE user_id=?;";
+                conn.query(sql, [name, email, photo, userId], function(err, results) {
+                    if (err) {
+                        console.log(err);
+                        callback(false);
+                    } else {
+                        console.log("UPDATE: ", results);
+                        callback(true);
+                    }
+                });
             }else{ // new user
+                console.log("New user");
                 var sql = "INSERT user(user_id, name, email, photo, provider) VALUES(?, ?, ?, ?, ?);";
                 conn.query(sql, [userId, name, email, photo, provider], function(err, results) {
                     if (err) {
