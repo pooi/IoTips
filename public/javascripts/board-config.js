335e7fc6ce7d4d1911f36f2fce670337207a65ca
pooi <ldayou@naver.com>
2018-08-20 11:03:46 +0900

    Add features   * Designed board page

--- public/javascripts/board-config.js
+++ public/javascripts/board-config.js
@@ -20,6 +20,54 @@ function init(init_user) {
             supporter: null,
             auth: new Auth(),
 
+            items: [
+                {
+                    id: "1",
+                    title: "Title-1",
+                    photo: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg"
+                },
+                {
+                    id: "2",
+                    title: "Title-2",
+                    photo: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg"
+                },
+                {
+                    id: "3",
+                    title: "Title-3",
+                    photo: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg"
+                },
+                {
+                    id: "4",
+                    title: "Title-4",
+                    photo: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg"
+                },
+                {
+                    id: "5",
+                    title: "Title-5",
+                    photo: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg"
+                },
+                {
+                    id: "6",
+                    title: "Title-6",
+                    photo: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg"
+                },
+                {
+                    id: "7",
+                    title: "Title-7",
+                    photo: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg"
+                },
+                {
+                    id: "8",
+                    title: "Title-8",
+                    photo: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg"
+                },
+                {
+                    id: "9",
+                    title: "Title-9",
+                    photo: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg"
+                }
+            ]
+
         },
         methods:{
             onScroll (e) {
